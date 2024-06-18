87115da9d76d2038092dc6711da7f2d389046541
halilagin <halil.agin@gmail.com>
2024-06-18 02:00:06 +0100

    github secrets set

---
+++ infra/amplify.tf
@@ -0,0 +1,59 @@
+resource "aws_amplify_app" "hello_world_amplify" {
+  name       = var.app_name
+  repository = var.repository #This will be your reactjs project
+
+  access_token             = var.github_access_token
+  enable_branch_auto_build = true
+
+  # The default build_spec added by the Amplify Console for React.
+  build_spec = <<-EOT
+    version: 0.1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - cd frontend
+            - yarn install
+        build:
+          commands:
+            - yarn run build
+      artifacts:
+        baseDirectory: build
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOT
+
+  # The default rewrites and redirects added by the Amplify Console.
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/index.html"
+  }
+
+  environment_variables = {
+    VITE_COGNITO_USER_POOL_ID   = aws_cognito_user_pool.sculture.id
+		VITE_COGNITO_CLIENT_ID      = aws_cognito_user_pool_client.sculture_pool_client.id
+    Provisioned_by              = "Terraform"
+  }
+}
+
+resource "aws_amplify_branch" "amplify_branch" {
+  app_id            = aws_amplify_app.hello_world_amplify.id
+  branch_name       = var.branch_name
+  enable_auto_build = true
+}
+
+resource "aws_amplify_domain_association" "domain_association" {
+  app_id                = aws_amplify_app.hello_world_amplify.id
+  domain_name           = var.domain_name
+  wait_for_verification = false
+
+  sub_domain {
+    branch_name = aws_amplify_branch.amplify_branch.branch_name
+    prefix      = var.branch_name
+  }
+
+}
---
+++ infra/amplify_variables.tf
@@ -0,0 +1,33 @@
+#declare github_access_token in secrets.auto.tfvars
+#assign the  "ghp_<deleted>" to github_access_token in secrets.auto.tfvars	
+
+variable "github_access_token" {
+  type        = string
+  #description = "github token to connect github repo"
+	#default = "${TF_VARS_github_access_token}"
+}
+
+variable "repository" {
+  type        = string
+  description = "github repo url"
+  #default     = "git@github.com:halilagin/sculture_aws_cognito.git"
+	default = "https://github.com/halilagin/sculture_aws_cognito.git"
+}
+
+variable "app_name" {
+  type        = string
+  description = "AWS Amplify App Name"
+  default     = "hello-world"
+}
+
+variable "branch_name" {
+  type        = string
+  description = "AWS Amplify App Repo Branch Name"
+  default     = "main"
+}
+
+variable "domain_name" {
+  type        = string
+  default     = "amplify001.peralabs.co.uk" #change this to your custom domain
+  description = "AWS Amplify Domain Name"
+}
